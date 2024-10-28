0e0969efeb82f5edaa106b98e50f5b8a0565e6f3
BastianNoMas <bastian.ihh@gmail.com>
2024-10-28 17:08:23 -0300

    Revert "Modificacion Index"
    This reverts commit be2353726391c4809aa7337764a8bccf79533c08.

--- src/main/java/com/example/crud/controllers/HomeController.java
+++ src/main/java/com/example/crud/controllers/HomeController.java
@@ -22,7 +22,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String index() {
-        return "index";
+        return "redirect:/ver-productos";
     }
 
     @GetMapping("/ver-productos")
@@ -37,18 +37,19 @@ public class HomeController {
         return "cargar-producto";
     }
 
-    @PostMapping("/guardar")
-    public String guardar(@ModelAttribute Producto producto) {
-        System.out.println("Producto a guardar: " + producto);
-        try {
-            productoService.saveProducto(producto);
-            System.out.println("Producto guardado correctamente.");
-        } catch (Exception e) {
-            System.out.println("Error al guardar el producto: " + e.getMessage());
-            e.printStackTrace(); // Esto imprimirá el stack trace de la excepción
-        }
-        return "redirect:/ver-productos";
+@PostMapping("/guardar")
+public String guardar(@ModelAttribute Producto producto) {
+    System.out.println("Producto a guardar: " + producto);
+    try {
+        productoService.saveProducto(producto);
+        System.out.println("Producto guardado correctamente.");
+    } catch (Exception e) {
+        System.out.println("Error al guardar el producto: " + e.getMessage());
+        e.printStackTrace(); // Esto imprimirá el stack trace de la excepción
     }
+    return "redirect:/ver-productos";
+}
+
 
     @GetMapping("/editar/{id}")
     public String editar(@PathVariable Long id, Model model) {
